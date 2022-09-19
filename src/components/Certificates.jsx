84d0cb0198b71c0311013f48ddd09e6356476922
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-09-19 16:19:30 -0300

    feat: adiciona footer responsivo

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -3,24 +3,32 @@ export default function Certificates() {
     <>
       <div className="flex bg-neutral-900 p-5 w-auto h-auto justify-center items-center">
         <div class="grid grid-flow-col grid-rows-3 grid-cols-2 lg:grid-rows-2 lg:grid-cols-3 gap-5 justify-center items-center">
+          <a target="_blank" rel="noreferrer" href="https://www.credential.net/a796a356-b052-429b-bbb4-0f4e596d3cd5#gs.cd4k8o" class="">
+            <div>
+              <img className="rounded-xl hover:scale-[1.8] ease-in duration-300" src="./assets/images/front.png" alt="" loading="lazy" />
+            </div>
+          </a>
+          <a target="_blank" rel="noreferrer" href="https://www.credential.net/0d3fa486-03dc-4f1c-8e6e-40ad66cbeead#gs.cd2xna" class="">
+            <div>
+              <img className="rounded-xl hover:scale-[1.8] ease-in duration-300" src="./assets/images/fundamentos.png" alt="" loading="lazy" />
+            </div>
+          </a>
+          <a target="_blank" rel="noreferrer" href="https://www.efset.org/cert/VzAhjP" class="">
+            <div>
+              <img className="rounded-xl hover:scale-[1.8] ease-in duration-300" src="./assets/images/efset.png" alt="" loading="lazy" />
+            </div>
+          </a>
           <div>
-            <img className="rounded-2xl hover:scale-150 ease-in duration-300" src="./assets/images/foolproof.jpg" alt="" loading="lazy" />
+            <img className="rounded-xl hover:scale-[1.8] ease-in duration-300" src="./assets/images/Aimo.png" alt="" loading="lazy" />
           </div>
-          <div>
-            <img className="rounded-2xl hover:scale-150 ease-in duration-300" src="./assets/images/front.jpg" alt="" loading="lazy" />
-          </div>
-          <div>
-            <img className="rounded-2xl hover:scale-150 ease-in duration-300" src="./assets/images/front.jpg" alt="" loading="lazy" />
-          </div>
-          <div>
+          <a target="_blank" rel="noreferrer" href="https://files.wiseup.com/?url=https://online.wiseup.com/storage/user-certificates/70838f09c751d821bcade83fdd08b207.pdf&documentType=CERTIFICATE&title=Wise%20Up%20Online%20Certificate&text=I%20got%20my%20English%20certificate%20at%20Wise%20Up%20Online&documentImage=https://online.wiseup.com/storage/certificate/sample.jpg" class="">
+            <div>
+              <img className="rounded-xl hover:scale-[1.8] ease-in duration-300" src="./assets/images/wise.png" alt="" loading="lazy" />
+            </div>
+          </a>
+          {/* <div>
             <img className="rounded-2xl hover:scale-150 ease-in duration-300" src="./assets/images/fundamentos.jpg" alt="" loading="lazy" />
-          </div>
-          <div>
-            <img className="rounded-2xl hover:scale-150 ease-in duration-300" src="./assets/images/front.jpg" alt="" loading="lazy" />
-          </div>
-          <div>
-            <img className="rounded-2xl hover:scale-150 ease-in duration-300" src="./assets/images/fundamentos.jpg" alt="" loading="lazy" />
-          </div>
+          </div> */}
         </div>
       </div>
     </>
